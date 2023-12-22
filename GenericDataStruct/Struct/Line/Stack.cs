463a125320cab1a982048dcc1e3a15b63e663f1f
wumeiyao <2695767451@qq.com>
2023-12-22 13:04:28 +0800

    add code

--- GenericDataStruct/Struct/Line/Stack.cs
+++ GenericDataStruct/Struct/Line/Stack.cs
@@ -1,12 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿/*
+ *
+ */
+using System;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace GenericDataStruct.Struct.Line
 {
-    internal class Stack
+    //
+    public class Stact<T>
     {
+        private LinkList<T> m_link;
+        public Stact()
+        {
+            m_link = new LinkList<T>();
+        }
+        public int Count()
+        {
+            return m_link.Count();
+        }
+        public T Popup()
+        {
+            if (m_link.Count() == 0)
+                return default;
+            return m_link.RemoveAt(m_link.Count() - 1);
+        }
+        public void EnQueue(T t)
+        {
+            m_link.Append(t);
+        }
+        public void Clear()
+        {
+            m_link.Clear();
+        }
     }
 }
